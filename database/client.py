be96eff45d77590a755a9d85af61e90e8dcdaea4
shashwat suvigyan <60818911+shashwatsuvigyan@users.noreply.github.com>
2026-01-11 15:39:05 +0530

    Update client.py

--- database/client.py
+++ database/client.py
@@ -1,5 +1,6 @@
 import motor.motor_asyncio
 from config import MONGO_URI
 
+# Initialize the Async Client
 cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
-db = cli.MyRoseBot
+db = cli.MyRoseBot  # Database Name
