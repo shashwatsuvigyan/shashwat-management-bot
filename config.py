77c13653820718a84fb40f7ebfd8d6d71d4a8659
shashwat suvigyan <60818911+shashwatsuvigyan@users.noreply.github.com>
2026-01-11 15:36:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
 
-# Load variables directly from the environment (Google Cloud settings)
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 OWNER_ID = int(os.environ.get("OWNER_ID", 0))
 MONGO_URI = os.environ.get("MONGO_URI")
-
-# Optional: Add other variables here if your bot needs them
 NSFW_API_USER = os.environ.get("NSFW_API_USER")
 NSFW_API_SECRET = os.environ.get("NSFW_API_SECRET")
