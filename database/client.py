d7d6b60fa8948e8549423cfa9927f42ad6ed5666
shashwat suvigyan <60818911+shashwatsuvigyan@users.noreply.github.com>
2026-01-11 15:37:27 +0530

    Update client.py

--- database/client.py
+++ database/client.py
@@ -1,6 +1,5 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+import motor.motor_asyncio
 from config import MONGO_URI
 
-# Motor is lazy; it won't connect until the first query
-client = AsyncIOMotorClient(MONGO_URI)
-db = client.rose_bot_db
+cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+db = cli.MyRoseBot
